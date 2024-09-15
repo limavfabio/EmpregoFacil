19d443fef518c799c595578c8ce03be2eed85249
Fábio Vinícius Lima <limavfabio@gmail.com>
2024-09-15 17:00:10 -0300

    custom_date helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def custom_date(datetime)
+    return "just now" if datetime > Time.current
+    distance_in_minutes = ((Time.current - datetime) / 60).to_i
+
+    case distance_in_minutes
+    when 0..59
+      "#{distance_in_minutes} minute#{'s' if distance_in_minutes != 1} ago"
+    when 60..1439
+      hours = distance_in_minutes / 60
+      "#{hours} hour#{'s' if hours != 1} ago"
+    when 1440..2879
+      "yesterday"
+    else
+      datetime.strftime("%m/%d")
+    end
+  end
 end
