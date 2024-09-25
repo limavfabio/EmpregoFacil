5b2c4a3115cdf40f0452a76afba0ee9f3cb46150
Fábio Vinícius Lima <limavfabio@gmail.com>
2024-09-25 11:22:50 -0300

    Dropdown for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -54,7 +54,7 @@ class CommentsController < ApplicationController
     @comment.destroy!
 
     respond_to do |format|
-      format.html { redirect_to comments_url, notice: "Comment was successfully destroyed." }
+      format.html { redirect_back_or_to post_path(@comment.post), notice: "Comment was successfully destroyed." }
       format.json { head :no_content }
     end
   end
