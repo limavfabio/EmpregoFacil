f2caccfa7fd7c6ec32f82d42ef854f8cd0132994
Fábio Vinícius Lima <limavfabio@gmail.com>
2024-09-27 12:12:23 -0300

    Avoid losing data on add experience dialog

--- app/javascript/controllers/dialog_controller.js
+++ app/javascript/controllers/dialog_controller.js
@@ -1,14 +1,48 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="dialog"
 export default class extends Controller {
-  static targets = ["dialog"]
-  
+  static targets = ["dialog", "denyClose"];
+
+  connect() {
+    if (this.hasDenyCloseTarget) {
+      this.dialogTarget.addEventListener("sl-request-close", (event) => {
+        // Check for unfilled inputs
+        const inputs = this.dialogTarget.querySelectorAll(
+          'input[data-confirm-discard="true"], textarea[data-confirm-discard="true"]'
+        );
+        const hasUnsavedChanges = Array.from(inputs).some(
+          (input) => input.value !== ""
+        );
+
+        if (hasUnsavedChanges) {
+          event.preventDefault(); // Prevent the default close action
+
+          // Show a confirmation dialog if there are unfilled inputs
+          const userConfirmed = confirm(
+            "You have unsaved changes. Do you really want to close the form?"
+          );
+
+          if (userConfirmed) {
+            // If the user chooses to close the form, dispatch a custom event to close it
+            this.hide();
+          }
+        }
+      });
+    }
+  }
+
   show() {
-    this.dialogTarget.show()
+    this.dialogTarget.show();
   }
 
   hide() {
-    this.dialogTarget.hide()
+    this.dialogTarget.hide();
+    const inputs = this.dialogTarget.querySelectorAll(
+      'input[data-confirm-discard="true"], textarea[data-confirm-discard="true"]'
+    );
+    inputs.forEach((input) => {
+      input.value = "";
+    });
   }
 }
